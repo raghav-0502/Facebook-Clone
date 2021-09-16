85e657cfd7d02ca30773ea916cecd03c1a757c92
raghav-0502 <you@example.com>
2021-09-17 02:45:59 +0530

    Created Facebook Box Component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Bootstrap/Header';
+import FBBox from './FBBox';
 
 class App extends Component {
   render() { 
     return (
       <div className="App">
         <Header className="Header">Facebook Clone</Header>
+        <div className="container">
+          <div className="col-6 offset-3">
+            <FBBox>Hello</FBBox>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/Components/FBBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FBBox = ({children, className}) => {
+    return (
+         <div className={"FBBox" + (className ? " " + className : "")}>
+            {children}
+         </div>
+    );
+};
+
+
+export default FBBox;
